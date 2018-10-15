3cff1d4e80c1165e3c01071b04ac316dd35e7c72
wl <wl@gmail.com>
2018-10-15 18:57:29 +0800

    modify

--- src/main/java/lambdasinaction/chap6/Test6.java
+++ src/main/java/lambdasinaction/chap6/Test6.java
@@ -19,6 +19,11 @@ public class Test6 {
                 .collect(partitioningBy(Test6::isPrime));
     }
 
+    public static boolean isPrime(List<Integer> primes, int candidate) {
+        return primes.stream()
+                .noneMatch(i -> candidate % i == 0);
+    }
+
     public static boolean isPrime(int candidate) {
         int candidateRoot = (int) Math.sqrt((double) candidate);
         return IntStream.rangeClosed(2, candidateRoot)
