77fc2be0dfb62e0121fba89d8a7ec7db0bf675fb
wl <wl@gmail.com>
2018-10-16 17:54:59 +0800

    modify

---
+++ src/main/java/skill/common/BigDecimalTutorial/Demo1.java
@@ -0,0 +1,33 @@
+package skill.common.BigDecimalTutorial;
+
+import java.math.BigDecimal;
+
+/*
+* BigDecimal to string
+* https://stackoverflow.com/questions/13900204/bigdecimal-to-string
+* */
+public class Demo1 {
+    public static void main(String[] args) {
+//        test1();
+        test2();
+    }
+
+    public static void test1() {
+        BigDecimal bdec = new BigDecimal(1.2345);
+        System.out.println(bdec.toString());// 1.2344999999999999307220832633902318775653839111328125
+    }
+
+
+    /*
+    * The problem with new BigDecimal(10.0001) is that the argument is a double and it happens that doubles can't represent 10.0001 exactly. So 10.0001 is "transformed" to the closest possible double, which is 10.000099999999999766941982670687139034271240234375 and that's what your BigDecimal shows.
+    For that reason, it rarely makes sense to use the double constructor.
+    You can read more about it here, Moving decimal places over in a double
+    *
+    * */
+    public static void test2(){
+        BigDecimal bdec1 = new BigDecimal("1.2345");
+        System.out.println(bdec1.toString());
+        BigDecimal bdec2 = BigDecimal.valueOf(1.2345);
+        System.out.println(bdec2.toString());
+    }
+}
--- src/main/java/skill/common/ChineseCharactersSize/Demo1.java
+++ src/main/java/skill/common/ChineseCharactersSize/Demo1.java
@@ -1,41 +1,49 @@
 package skill.common.ChineseCharactersSize;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+
 /*
-* Java一个汉字占几个字节（详解与原理）(转载)
-* https://www.cnblogs.com/lslk89/p/6898526.html
-* */
+ * Java一个汉字占几个字节（详解与原理）(转载)
+ * https://www.cnblogs.com/lslk89/p/6898526.html
+ * */
 public class Demo1 {
     public static void main(String[] args) {
-        test1();
-        test2();
-        test3();
+//        test1();
+//        test2();
+//        test3();
+        test4();
     }
 
     public static void test1() {
         String a = "名";
         try {
-            System.out.println("UTF-8编码长度:"+a.getBytes("UTF-8").length);
+            System.out.println("UTF-8编码长度:" + a.getBytes("UTF-8").length);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
 
-    public static void test2(){
+    public static void test2() {
         String a = "名";
         try {
-            System.out.println("GBK编码长度:"+a.getBytes("GBK").length);
+            System.out.println("GBK编码长度:" + a.getBytes("GBK").length);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
 
-    public static void test3(){
+    public static void test3() {
         String a = "名";
         try {
-            System.out.println("GB2312编码长度:"+a.getBytes("GB2312").length);
+            System.out.println("GB2312编码长度:" + a.getBytes("GB2312").length);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void test4() {
+        BigDecimal bdec = new BigDecimal("0");
+        System.out.println(bdec.toString());
     }
 }
