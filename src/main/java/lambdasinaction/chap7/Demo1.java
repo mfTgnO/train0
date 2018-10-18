5b99dea0db6fc36d13c32a835a1875a8b50941ab
wl <wl@gmail.com>
2018-10-18 09:01:21 +0800

    modify

--- src/main/java/lambdasinaction/chap7/Demo1.java
+++ src/main/java/lambdasinaction/chap7/Demo1.java
@@ -1,16 +1,38 @@
 package lambdasinaction.chap7;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
+
+import static lambdasinaction.chap7.ForkJoinSumCalculator.forkJoinSum;
 
 public class Demo1 {
     public static void main(String[] args) {
-        long l = iterativeSum(10);
-        System.out.println(l);
+        /*long l = iterativeSum(10);
+        System.out.println(l);*/
+//        test2();
+//        test3();
+        test4();
     }
 
     public static void test1() {
 //        System.out.println("Sequential sum done in: " + measureSumPerf(ParallelStreams::sequentialSum, 10_000_000) + " msecs");
 //        System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + " msecs");
+    }
+
+    public static void test2() {
+        long l = forkJoinSum(20000000);
+        System.out.println(l);
+    }
+
+    public static void test3() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        int poolSize = forkJoinPool.getPoolSize();
+        System.out.println(poolSize);
+    }
+
+    public static void test4() {
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        System.out.println(availableProcessors);
     }
 
     public static long iterativeSum(long n) {
---
+++ src/main/java/lambdasinaction/chap7/ForkJoinSumCalculator.java
@@ -0,0 +1,52 @@
+package lambdasinaction.chap7;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
+
+public class ForkJoinSumCalculator extends RecursiveTask<Long> {
+    public static final long THRESHOLD = 10_000;
+    public final long[] numbers;
+    public final int start;
+    public final int end;
+
+    public ForkJoinSumCalculator(long[] numbers) {
+        this(numbers, 0, numbers.length);
+    }
+
+    private ForkJoinSumCalculator(long[] numbers, int start, int end) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        int length = end - start;
+        if (length <= THRESHOLD) {
+            return computeSequentially();
+        }
+        System.out.println("here");
+        ForkJoinSumCalculator leftTask = new ForkJoinSumCalculator(numbers, start, start + length / 2);
+        leftTask.fork();
+        ForkJoinSumCalculator rightTask = new ForkJoinSumCalculator(numbers, start + length / 2, end);
+        Long rightResult = rightTask.compute();
+        Long leftResult = leftTask.join();
+        return leftResult + rightResult;
+    }
+
+    private long computeSequentially() {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    public static long forkJoinSum(long n) {
+        long[] numbers = LongStream.rangeClosed(1, n)
+                .toArray();
+        ForkJoinSumCalculator task = new ForkJoinSumCalculator(numbers);
+        return new ForkJoinPool().invoke(task);
+    }
+}
