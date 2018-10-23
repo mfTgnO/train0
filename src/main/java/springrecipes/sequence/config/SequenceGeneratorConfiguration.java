aac74b2149ea05bb05c93efafddfd69b9cf04aa5
tiemj <tiemj@outlook.com>
2018-10-23 21:54:09 +0800

    Reducing and summarizing

--- src/main/java/springrecipes/sequence/config/SequenceGeneratorConfiguration.java
+++ src/main/java/springrecipes/sequence/config/SequenceGeneratorConfiguration.java
@@ -1,4 +1,19 @@
 package springrecipes.sequence.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+@ComponentScan(
+        includeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.REGEX,
+                        pattern = {"springrecipes.sequence.*Dao",
+                                "springrecipes.sequence.*Service"})
+        },
+        excludeFilters = {@ComponentScan.Filter(
+                type = FilterType.ANNOTATION,
+                classes = {org.springframework.stereotype.Controller.class}
+
+        )})
 public class SequenceGeneratorConfiguration {
 }
