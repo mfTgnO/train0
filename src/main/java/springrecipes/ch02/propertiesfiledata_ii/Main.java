6944e5316b55ffa3bf2e2c1b011a851d87ec8b44
tiemj <tiemj@outlook.com>
2018-10-25 22:39:38 +0800

    Create a Java Config for Your POJO

--- src/main/java/springrecipes/ch02/propertiesfiledata_ii/Main.java
+++ src/main/java/springrecipes/ch02/propertiesfiledata_ii/Main.java
@@ -1,10 +1,16 @@
 package springrecipes.ch02.propertiesfiledata_ii;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import springrecipes.ch02.propertiesfiledata_ii.config.ShopConfiguration;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ShopConfiguration.class);
 
         Product aaa = context.getBean("aaa", Product.class);
@@ -19,5 +25,11 @@ public class Main {
         ShoppingCart cart2 = context.getBean("shoppingCart", ShoppingCart.class);
         cart2.addItem(dvdrw);
         System.out.println("Shopping cart 2 contains " + cart2.getItems());
+
+
+        Resource resource = new ClassPathResource("discounts.properties");
+        Properties props = PropertiesLoaderUtils.loadProperties(resource);
+        System.out.println("And don't forget our discounts!");
+        System.out.println(props);
     }
 }
