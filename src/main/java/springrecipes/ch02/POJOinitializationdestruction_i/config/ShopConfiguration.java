f81f4ce5b98e98a504e0f6c7922308aa44782438
wl <wl@gmail.com>
2018-10-26 11:54:50 +0800

    modify

---
+++ src/main/java/springrecipes/ch02/POJOinitializationdestruction_i/config/ShopConfiguration.java
@@ -0,0 +1,45 @@
+package springrecipes.ch02.POJOinitializationdestruction_i.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springrecipes.ch02.POJOinitializationdestruction_i.Battery;
+import springrecipes.ch02.POJOinitializationdestruction_i.Cashier;
+import springrecipes.ch02.POJOinitializationdestruction_i.Disc;
+import springrecipes.ch02.POJOinitializationdestruction_i.Product;
+
+@Configuration
+@ComponentScan("springrecipes.ch02.POJOinitializationdestruction_i")
+public class ShopConfiguration {
+    @Bean
+    public Product aaa() {
+        Battery p1 = new Battery();
+        p1.setName("AAA");
+        p1.setPrice(2.5);
+        p1.setRechargeable(true);
+        return p1;
+    }
+
+    @Bean
+    public Product cdrw() {
+        Disc p2 = new Disc("CD-RW", 1.5);
+        p2.setCapacity(700);
+        return p2;
+    }
+
+    @Bean
+    public Product dvdrw() {
+        Disc p2 = new Disc("DVD-RW", 3.0);
+        p2.setCapacity(700);
+        return p2;
+    }
+
+    @Bean(initMethod = "openFile", destroyMethod = "closeFile")
+    public Cashier cashier() {
+        String path = System.getProperty("java.io.tmpdir") + "/cashier";
+        Cashier cashier = new Cashier();
+        cashier.setFileName("checkout");
+        cashier.setPath(path);
+        return cashier;
+    }
+}
