bda30f54ac041b126b7df60e06817e7b3cf57c7f
wl <wl@gmail.com>
2018-10-26 18:32:49 +0800

    modify

---
+++ src/main/java/springrecipes/ch02/createPOJOswithfactory_i/Disc.java
@@ -0,0 +1,20 @@
+package springrecipes.ch02.createPOJOswithfactory_i;
+
+public class Disc extends Product {
+    private int capacity;
+
+    public Disc() {
+    }
+
+    public Disc(String name, double price) {
+        super(name, price);
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
