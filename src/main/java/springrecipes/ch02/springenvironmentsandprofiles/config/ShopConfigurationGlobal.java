02d3af3ebc0c08c9d2ce0dd38d9a6c45d2178bfb
wl <wl@gmail.com>
2018-10-27 10:15:52 +0800

    modify

---
+++ src/main/java/springrecipes/ch02/springenvironmentsandprofiles/config/ShopConfigurationGlobal.java
@@ -0,0 +1,19 @@
+package springrecipes.ch02.springenvironmentsandprofiles.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springrecipes.ch02.springenvironmentsandprofiles.Cashier;
+
+@Configuration
+@Profile("global")
+public class ShopConfigurationGlobal {
+    @Bean(initMethod = "openFile", destroyMethod = "closeFile")
+    public Cashier cashier() {
+        final String path = System.getProperty("java.io.tmpdir") + "cashier";
+        Cashier c1 = new Cashier();
+        c1.setFileName("checkout");
+        c1.setPath(path);
+        return c1;
+    }
+}
