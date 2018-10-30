df27eef1be532153c131403fe0310b965f060e69
wl <wl@gmail.com>
2018-10-30 18:52:37 +0800

    modify

---
+++ src/main/java/springrecipes/ch03/developasimplewebapplication_i/service/ReservationService.java
@@ -0,0 +1,9 @@
+package springrecipes.ch03.developasimplewebapplication_i.service;
+
+import springrecipes.ch03.developasimplewebapplication_i.domain.Reservation;
+
+import java.util.List;
+
+public interface ReservationService {
+    List<Reservation> query(String courtName);
+}
