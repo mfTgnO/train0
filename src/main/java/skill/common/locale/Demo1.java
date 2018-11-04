d4fa5cf840ceeb3d39192b2e55609f2fe0731e70
tiemj <tiemj@outlook.com>
2018-11-05 07:09:46 +0800

    create ch03 title

---
+++ src/main/java/skill/common/locale/Demo1.java
@@ -0,0 +1,10 @@
+package skill.common.locale;
+
+import java.util.Locale;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Locale en = new Locale("en");
+        System.out.println(en);
+    }
+}
