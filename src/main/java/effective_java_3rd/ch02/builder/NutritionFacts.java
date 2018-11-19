4e00071a51bdacf63941f2a028408b29c85d2136
wl <wl@gmail.com>
2018-11-19 15:38:14 +0800

    modify

---
+++ src/main/java/effective_java_3rd/ch02/builder/NutritionFacts.java
@@ -0,0 +1,69 @@
+package effective_java_3rd.ch02.builder;
+
+// Builder Pattern  (Page 13)
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbodydrate;
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters - initialized to default values
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbodydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbodydrate(int val) {
+            carbodydrate = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    public NutritionFacts(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbodydrate = builder.carbodydrate;
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbodydrate(27)
+                .build();
+    }
+}
---
+++ src/main/java/effective_java_3rd/ch02/hierarchicalbuilder/PizzaTest.java
@@ -0,0 +1,19 @@
+package effective_java_3rd.ch02.hierarchicalbuilder;
+
+// Using the hierarchical builder (Page 16)
+public class PizzaTest {
+    public static void main(String[] args) {
+        NyPizza nyPizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE)
+                .addTopping(Pizza.Topping.ONION)
+                .build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(Pizza.Topping.HAM)
+                .sauseInside()
+                .build();
+
+        System.out.println(nyPizza);
+        System.out.println(calzone);
+    }
+}
