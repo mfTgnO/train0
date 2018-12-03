0dd9997a11002021cea72a9db4730a51e5062408
wl <wl@gmail.com>
2018-12-03 14:45:34 +0800

    modify

---
+++ src/main/java/effective_java_3rd/ch3/autovalue/Animal.java
@@ -0,0 +1,16 @@
+/*
+package effective_java_3rd.ch3.autovalue;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+abstract class Animal {
+    static Animal create(String name, int numberOfLegs) {
+        return new AutoValue_Animal(name, numberOfLegs);
+    }
+
+    abstract String name();
+
+    abstract int numberOfLegs();
+}
+*/
