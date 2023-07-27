53dfb2b4e554a55f61f9c10a48bc36bd57d0ab55
Pariloga <pariloga@example.com>
2023-07-27 15:02:41 +0800

    Serializer Demo

---
+++ src/test/java/com/example/demo/serializer/SerializerDemo.java
@@ -0,0 +1,17 @@
+package com.example.demo.serializer;
+
+import java.util.PrimitiveIterator;
+import org.junit.Test;
+
+/**
+ * @author:
+ * @createDate: 2023-07-13 17:20
+ * @description:
+ */
+public class SerializerDemo {
+    @Test
+    public void test1() {
+        System.out.println(123);
+
+    }
+}
